427df0772c968127500afff4c339e42bfdb13864
kothale19 <kothale.pujaprasad@gmail.com>
2022-12-23 13:54:55 +0530

    Added: Program of Leap Year

--- src/com/blz/day5coreprogram/FlipCoin.java
+++ src/com/blz/day5coreprogram/FlipCoin.java
@@ -6,42 +6,20 @@ public class FlipCoin {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Core Program!!");
-		
-		int heads = 0;
-		 int tails = 0;
-		 int counts = 1;
-		 double random = 0.0;
-		 
-		 System.out.println("Enter the number:");
-		 Scanner n = new Scanner(System.in);
-		 int flips = n.nextInt();
-		 
-		 while(counts <= flips) {
-			 random = Math.random();
-			 System.out.println(counts+" "+random);
-			 
-			 if(random < 0.5) {
-				 heads++;
-			System.out.println("heads");
-			                  }
-			 
-			 else {
-				 tails++;
-			 System.out.println("tails");
-		          }
-		         counts++;
-		              
-		                       }
-	    
-	     System.out.println("Number of Heads: "+heads);
-	     System.out.println("Number of Tails: "+tails);
-	     
-	     double headPercentage = (double)heads/flips * 100;
-	     double tailPercentage = (double)tails/flips * 100;
-	     
-	     System.out.println("Percentage of Heads: "+headPercentage+"%");
-	     System.out.println("Percentage if Tails: "+tailPercentage+"%");
-	     
+
+		int year;
+
+		System.out.println("Enter the Year:");
+		Scanner scanner = new Scanner(System.in);
+		year = scanner.nextInt();
+
+		if (year % 100 == 0 && year % 400 == 0 || year % 100 != 0 && year % 4 == 0) {
+			System.out.println("It is a LeapYear");
+		}
+
+		else {
+			System.out.println("It is not a LeapYear");
+		}
 	}
 
 }
