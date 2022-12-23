06e9f5963082b669a7cf05d425cada453ef0720d
kothale19 <kothale.pujaprasad@gmail.com>
2022-12-23 13:42:26 +0530

    Added : welcome mesage

---
+++ src/com/blz/day5coreprogram/FlipCoin.java
@@ -0,0 +1,11 @@
+package com.blz.day5coreprogram;
+
+import java.util.Scanner;
+
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Core Program!!");
+	}
+
+}
